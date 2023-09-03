e791018515daa24e9d02328fb93b131e8bb20c15
Tisca <t.scaffidi@hotmail.com>
2023-09-03 14:09:56 +0200

    Solved JS challenge

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 const epicMix = [
-  "Time to Pretend",
-  "O-o-oh Child",
-  "Wish You Were Here",
-  "Heroes",
-  "I Put a Spell on You",
-  "Call Me",
+  "Unfinished Sympathy",
+  "Q.U.E.E.N.",
+  "Gold Dust",
+  "Fallin'",
+  "One Day I'll Fly Away",
+  "Iris",
+  "Climbatize",
+  "golden hour",
+  "(Where Do I Begin) Love Story",
+  "(Don't) Give Hate a Chance",
+  "Kiss from a Rose",
   "Paper Planes",
-  "Jolene",
-  "You Don't Own Me",
+  "***Flawless",
+  "Take a Bow",
   "Fast Car",
   "Run the World (Girls)",
-  "Superstition"
+  "Stop Me",
+  "The Garden",
+  "Sleep Deprivation",
+  "Transient"
 ];
 
 const mixList = document.querySelector(".mix");
@@ -18,6 +26,18 @@ const button = document.querySelector(".show-list");
 const total = document.querySelector(".total");
 
 button.addEventListener("click", function () {
+  mixInfo(epicMix);
   mixList.classList.remove("hide");
   button.classList.add("hide");
 });
+
+total.innerText = `${epicMix.length} great songs!`;
+
+const mixInfo = function (mix) {
+  mix.forEach(function(song, index) {
+    let li = document.createElement("li");
+    li.classList.add("song");
+    li.innerHTML = `<span class="song-number">#${index + 1}</span> ${song}`;
+    mixList.append(li);
+  });
+};
